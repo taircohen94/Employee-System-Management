ad8b66217633988fe996a5c340f279088f3e34a5
taircohen94 <taircohen94@gmail.com>
2021-09-12 17:09:55 +0300

    init

---
+++ src/main/java/com/example/Employee/System/EmployeeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Employee.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSystemApplication.class, args);
+	}
+
+}
